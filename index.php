2f7f8e2b9633c29632a6f71aaaf12f4bef20c7fd
oscarmiguel13 <carlo11171977@gmail.com>
2022-03-05 08:54:40 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+    <body>
+        <?php echo "Hello, World! I'm Carlo.";
+            //this is a single line comment
+            /*this is 
+             multi line
+             comment
+             */
+         ?>
+        <a href="welcome.php">click here</a>
+    </body>
+</html>
