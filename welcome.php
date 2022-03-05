fff97476abfaf93d86eda2bfc5fa168532094b10
oscarmiguel13 <carlo11171977@gmail.com>
2022-03-05 08:55:07 +0800

    Create welcome.php

---
+++ welcome.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>Welcome!</title>
+</head>
+        <body>
+            <?php echo "Hello, I'm Carlo. I'm new to git. How about you?"; ?>
+            <a href="index.php">home</a>
+        </body>
+</html>
