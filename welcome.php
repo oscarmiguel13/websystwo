36af5fec15973446011f1846fd88f707c13904cb
oscarmiguel <carlo11171977@gmail.com>
2022-03-08 10:33:16 +0800

    git commit welcome.php -m "added Im new to git msg"

--- welcome.php
+++ welcome.php
@@ -3,7 +3,7 @@
 <title>Welcome!</title>
 </head>
         <body>
-            <?php echo "Hello, I'm Carlo. I'm new to git. How about you?"; ?>
+            <?php echo "Hello, I'm Oscar Miguel. I'm new to git. How about you?"; ?>
             <a href="index.php">home</a>
         </body>
 </html>
